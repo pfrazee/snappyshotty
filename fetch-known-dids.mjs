f55f75d2c304e9a92a9ae743592459c49208c167
Paul Frazee <pfrazee@gmail.com>
2025-06-18 16:19:12 -0700

    Update path usage

--- fetch-known-dids.mjs
+++ fetch-known-dids.mjs
@@ -1,4 +1,4 @@
-import { RELAY1_WEST } from './src/const.mjs'
+import { RELAY1_WEST, REPOS_JSON } from './src/const.mjs'
 import { fetchKnownBlueskyDids } from './src/repos.mjs'
 import fs from 'node:fs'
 
@@ -6,9 +6,9 @@ console.log('Fetching list of all known DIDs from', RELAY1_WEST)
 const dids = await fetchKnownBlueskyDids(RELAY1_WEST, (n) =>
   console.log(n, 'DIDs fetched')
 )
-console.log('Fetch completed, writing to ./.repos.json')
+console.log('Fetch completed, writing to', REPOS_JSON)
 fs.writeFileSync(
-  './.repos.json',
+  REPOS_JSON,
   JSON.stringify({ createdAt: new Date().toISOString(), dids }),
   'utf8'
 )
